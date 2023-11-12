aceaaf8948a991eb66a529132b28d87638ddb6d7
mingmingmon <mingmingmon@naver.com>
2023-11-12 15:53:23 +0900

    mingming branch test

--- TestClass.java
+++ TestClass.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("hi from local ");
+        System.out.println("hi i want to upload to mingming branch");
     }
 }
